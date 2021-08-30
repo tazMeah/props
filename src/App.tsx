9ea4cccb248e78ef968b05645f77cdb9cfa43f2f
Montaz Meah <montaz.meah@mrm.com>
2021-08-30 19:21:45 -0400

    pushing again because maybe no code??

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="App">
+			<div>
+				<button
+					onClick={() => {
+						setCount((prv) => prv + 1);
+					}}
+				>
+					UP
+				</button>
+				<p>{count}</p>
+				<button
+					onClick={() => {
+						setCount((prv) => prv - 1);
+					}}
+				>
+					DOWN
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
